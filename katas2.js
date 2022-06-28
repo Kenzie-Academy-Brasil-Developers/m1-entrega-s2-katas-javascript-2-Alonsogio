19a6f7d08497cf185cc1e533feec2ff524902620
GiovannaAlonso <106785638+Alonsogio@users.noreply.github.com>
2022-06-28 19:07:48 -0300

    Katas02

--- katas2.js
+++ katas2.js
@@ -1,29 +1,52 @@
 // comece a criar a sua função add na linha abaixo
-
-
+function add(number1, number2) {
+    let soma = number1 + number2;
+    return soma;
+  }
 // descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
 // comece a criar a sua função multiply na linha abaixo
 
-
+function multiply(number1, number2) {
+    let multipli = 0;
+  
+    for (let i = 0; i < number2; i++) {
+      multipli = add(number1, multipli);
+    }
+    return multipli;
+  }
 // descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
+function power(x, n) {
+  let expo = 1;
 
+  for (let pot = 1; pot <= n; pot++) {
+    expo = multiply(expo,x);
+  }
+  return expo;
+}
 
 // descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+ console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
 // comece a criar a sua função factorial na linha abaixo
-
+function factorial(y) {
+    let fac = 1;
+  
+    for (let rial = 1; rial <= y; rial++) {
+      fac = multiply(fac, rial);
+    }
+    return fac;
+  }
 
 // descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
 
 /**
